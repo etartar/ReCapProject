b78da39ad879256793b82008d50ce98cb7fc255e
Emir TARTAR <emirtartar35@gmail.com>
2021-10-21 23:00:36 +0300

    DataAccess katmanın da Customer, Rental ve User için ilgili Abstract ve Concrete sınıflar oluşturuldu.

--- DataAccess/Abstract/ICarDal.cs
+++ DataAccess/Abstract/ICarDal.cs
@@ -1,9 +1,7 @@
 ﻿using Core.DataAccess;
 using Entities.Concrete;
 using Entities.DTOs;
-using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
